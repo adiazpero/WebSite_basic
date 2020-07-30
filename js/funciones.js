0cc37c0aa21c347e980fee0f40a979de306785b5
adiazpero <anitta_83@hotmail.com>
2020-07-30 12:48:16 +0200

    modificaciones html y css. Archivo js funciones

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,30 @@
-let ubicacionPrincipal = window.pageYOffset;
+let ubicacionPrincipal = window.pageYOffset; //0
+AOS.init();
+
+window.addEventListener("scroll", function() {
+    let desplazamientoActual = window.pageYOffset; //180
+    if (ubicacionPrincipal >= desplazamientoActual) { //200 > 180
+        document.getElementsByTagName("nav")[0].style.top = "0px";
+    } else {
+        document.getElementsByTagName("nav")[0].style.top = "100px";
+    }
+    ubicacionPrincipal = desplazamientoActual; //200
+})
+
+
+
+let linkHeader = document.querySelectorAll(".link-header")[0];
+let semaforo = true;
+
+document.querySelectorAll(".hamburguesa")[0].addEventListener("click", function() {
+    if (semaforo) {
+        document.querySelectorAll(".hamburguesa")[0].style.color = "#fff";
+        semaforo = false;
+    } else {
+        document.querySelectorAll(".hamburguesa")[0].style.color = "#fff";
+        semaforo = true;
+    }
+
+
+    linkHeader.classList.toggle
+})
